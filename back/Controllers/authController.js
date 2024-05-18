289790604feada05de73f5afedea32bdfde53f6f
warisamir <warisamir1918@gmail.com>
2024-05-19 02:52:05 +0530

    authorised user

--- back/Controllers/authController.js
+++ back/Controllers/authController.js
@@ -2,6 +2,13 @@ import User from '../models/UserSchema.js'
 import Doctor from '../models/DoctorSchema.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
+
+const generateToken=user=>{
+    return jwt.sign({id:user._id, role:user.role},
+        process.env.JWT_SECRET,
+        {expiresIn: process.env.JWT_EXPIRE}
+    )
+}
 export const register =async(req,res)=>{
     const {name,email,password,role, photo ,gender}=req.body;
     try{
@@ -34,15 +41,35 @@ export const register =async(req,res)=>{
     }
 }
 export const login =async(req,res)=>{
+    const {email}=req.body;
     try{
-        const {email,password}=req.body
-        const user =await User.findOne({email})
+        let user= null;
+         const patient =await User.findOne({email});
+        const  doctor=await Doctor.findOne({email});
+         if(patient)
+            user=patient;
+        else if(doctor)
+            user=doctor;
         if(!user){
-            return res.status(400).json({message:"user does not exist"})
+            return res.status(404).json({message:"user does not exist"})
         }
-        const isMatch =await bcrypt.compare(password,user.password)
+        const isMatch =await bcrypt.compare(
+            req.body.password,user.password);
+        if(!isMatch){
+            return res.status(400)
+            .json({status:false,message:"Invalid credentials"})
+            }
+            const token=generateToken(user);
+            const {password ,role ,appointments,...rest}=user._doc;
+            res.status(200).
+            json({
+                status:true,
+                message:"Login sucessful",
+                token,
+                data:{...rest},
+                role})
 }
 catch(err){
-    res.status(500).json({message:"Internal server error"})
+    res.status(500).json({message:"failed to login"})
 }
 }
