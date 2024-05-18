e57a7255fe78fe75d89939e7647a932693546dea
warisamir <warisamir1918@gmail.com>
2024-05-18 16:50:13 +0530

    signup page done

--- front/src/pages/Signup.jsx
+++ front/src/pages/Signup.jsx
@@ -1,8 +1,138 @@
-import React from 'react'
-
+import signupImg from '../assets/Image/signup.gif'
+import avatar from '../assets/Image/doctor-img01.png'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 const Signup = () => {
+const handleInputChange=e=>{
+ 
+  setformData({...formData,[e.target.name]:e.target.value});
+}
+
+const [selectedFile, setSelectedFile] = useState(null)
+
+const [formData, setformData] = useState({
+  name:'',
+  email:'',
+  password:'',
+  photo:'',
+  gender:'',
+  role:'patient'
+})
+
+const handleFileInputChange=async(event)=>{
+   const file =event.target.files[0];
+
+  //  console.log(file);
+  }
+  const submitHandler =async (event)=>{
+    event.preventDefault();
+  }
+
   return (
-    <div>Signup</div>
+    <section className='px-5 xl:px-0'>
+      <div className='max-w-[1170px] mx-auto'>
+        <div className='grid grid-cols-1 lg:grid-cols-2'>
+          <div className='hidden lg:block bg-primaryColor rounded-l-lg'>
+            <figure className='rounded-l-lg'>
+              <img src={signupImg} alt="" className='w-full rounded-l-lg'/>
+            </figure>
+          </div>
+          <div className='rounded-l-lg lg:pl-16'>
+            <h3 className='text-headingColor text-[22px] leading-9 font-bold mb-10'>
+            Create an <span className='text-primaryColor'>account</span></h3>
+            <form onSubmit={submitHandler}>
+          <div className="mb-5">
+          <input type="text"
+           placeholder="Full name"
+          name="name" 
+          onChange={handleInputChange} 
+          value={formData.name}
+          className="w-full  py-3 border-b border-solid
+          border-[#0066ff61] focus:outline-none
+          focus:border-b-primaryColor text-[16px] leading-7
+          text-headingColor placeholder:text-textColor 
+           cursor-pointer"
+          />
+        </div>
+        <div className="mb-5">
+          <input type="email "
+           placeholder="enter your email"
+          name="email" 
+          onChange={handleInputChange} 
+          value={formData.email}
+          className="w-full  py-3 border-b border-solid
+          border-[#0066ff61] focus:outline-none
+          focus:border-b-primaryColor text-[16px] leading-7
+          text-headingColor placeholder:text-textColor 
+           cursor-pointer"
+          /> </div> 
+            <div className="mb-5">
+          <input type="password"
+           placeholder="enter your password"
+          name="password" 
+          onChange={handleInputChange} 
+          value={formData.password}
+          className="w-full  py-3 border-b border-solid
+          border-[#0066ff61] focus:outline-none
+          focus:border-b-primaryColor text-[16px] leading-7
+          text-headingColor placeholder:text-textColor 
+           cursor-pointer"
+          />
+        </div>
+          <div className="mb-5 flex items-center justify-between">
+            <label  className='text-headingColor font-bold text-[16px] leading-7' >
+            Are you a:
+            <select name="role" 
+            onChange={handleInputChange} value={formData.role}
+            className='text-textColor font-semibold text-[15px] leading-7 px-4  py-3 focus:outline-none'>
+              <option value="patient">Patient</option>
+              <option value="doctor">Doctor</option>
+            </select>
+            </label>
+          </div>
+          <div className="mb-5 flex items-center justify-between">Gender:
+            <label className='text-headingColor font-bold text-[16px] leading-7' >
+            Are you a:
+            <select name="gender" 
+            onChange={handleInputChange} value={formData.gender}
+            className='text-textColor font-semibold text-[15px] leading-7 px-4  py-3 focus:outline-none'>
+              <option value="">Select</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="other">other</option>
+            </select>
+            </label>
+          </div>
+          <div  className='m-5 flex items-center gap-3'>
+            <figure className='w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor
+            flex items-center justify-center'>
+              <img src={avatar}  alt="" className='w-full rounded-full'/>
+            </figure>
+            <div className='relative w-[160px] h-[50px] '>
+              <input type="file" name="photo" id="customFile"
+              onChange={handleFileInputChange}
+              accept=".jpg,.png"  className='absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer'/>
+              <label htmlFor='customFile' className='absolute top-0 w-full left-0 h-full flex item-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden
+              bg-[#0066ff46] text-handlingColor font-semibold rounded-lg truncate cursor-pointer'>
+                Upload photo 
+              </label>
+            </div>
+          </div>
+
+          <div className="mt-7">
+          <button type="submit" className="w-full bg-primaryColor text-white text-[18px] 
+          leading-[30px] rounded-lg">
+            Signup 
+          </button>
+        </div>
+          <p className="mt-5 text-textColor ">
+          Already have an acount? <Link to='/login' className="text-primaryColor font-medium ml-1" >Login</Link>
+        </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
